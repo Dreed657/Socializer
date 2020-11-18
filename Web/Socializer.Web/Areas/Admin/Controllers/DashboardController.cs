e4d905055677a6c6175db78516990fa828f64eb6
Stoyan Lazarov <ninjakiviWTF@gmail.com>
2020-11-18 19:23:16 +0200

    Add template for dashboard
    Add 403 custom error page

--- Web/Socializer.Web/Areas/Admin/Controllers/DashboardController.cs
+++ Web/Socializer.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -1,13 +1,21 @@
 ﻿namespace Socializer.Web.Areas.Admin.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Socializer.Common;
 
-    [Route("/admin/[Controller]")]
+    [Area("Admin")]
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     public class DashboardController : Controller
     {
         public IActionResult Index()
         {
-            return this.Ok("This is the dashboard");
+            return this.View();
+        }
+
+        public IActionResult Error()
+        {
+            return this.View();
         }
     }
 }
