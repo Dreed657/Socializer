5214bd8285293b96d9ada806dde7d2cd111dc93f
Stoyan Lazarov <ninjakiviWTF@gmail.com>
2020-11-24 19:03:16 +0200

    Dashboard user profile page

--- Web/Socializer.Web/Areas/Admin/Controllers/UsersController.cs
+++ Web/Socializer.Web/Areas/Admin/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Socializer.Services.Data.Profiles;
 using Socializer.Services.Data.Users;
 using Socializer.Web.ViewModels.Users;
 
@@ -8,10 +9,18 @@ namespace Socializer.Web.Areas.Admin.Controllers
     public class UsersController : DashboardController
     {
         private readonly IUserService userService;
+        private readonly IProfilesService profilesService;
 
-        public UsersController(IUserService userService)
+        public UsersController(IUserService userService, IProfilesService profilesService)
         {
             this.userService = userService;
+            this.profilesService = profilesService;
+        }
+
+        public async Task<IActionResult> Index(string username)
+        {
+            var model = await this.profilesService.GetProfileByUsername(username);
+            return this.View(model);
         }
 
         public async Task<IActionResult> AllUsers()
